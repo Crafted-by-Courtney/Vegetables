072edc287410f26b9efe9df0506283351197fbfe
Courtney <courtneycook82@gmail.com>
2023-09-22 12:43:14 -0400

    first commit

---
+++ models/vegetables.js
@@ -0,0 +1,27 @@
+const vegetables = [{
+    name:'radish',
+    color: 'red',
+    readyToEat: true
+},
+{
+    name:'zuccini',
+    color: 'green',
+    readyToEat: false
+},
+{
+    name:'cucumber',
+    color: 'green',
+    readyToEat: false
+},
+{
+    name:'bell pepper',
+    color: 'green',
+    readyToEat: false
+},
+{
+    name:'squash',
+    color: 'yellow',
+    readyToEat: true
+}];
+
+module.exports = vegetables;
